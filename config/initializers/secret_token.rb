0ef0b1e461ea2c7d72a6326097b36839a2b9dbd8
Robert Wood <woodrobe@gmail.com>
2012-08-24 13:41:15 -0700

    Initial commit wooddemo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wooddemo::Application.config.secret_token = 'c0d9e2ad52942fa7c1a326ef7a1b3a7ce2de2bce94d8a55c9fe44b776f514e3435608869c3343e42abb9ce6acd3108ef3ed40fa21e4347e4cf22202453f101be'
